d2af60a851c9a3cd0ce6f301d9272cb3c075dfba
jus-schl <justus.schlueter@gmx.de>
2024-11-29 11:26:45 +0100

    Beta version

--- task1-template/pages/2_stats.py
+++ task1-template/pages/2_stats.py
@@ -1,17 +1,22 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pdstre
+import pandas as pd
 
 st.title( "Statistics Page")
+if len(st.session_state.distance_country) > 0:
+    st.subheader(f"Your last guess was {st.session_state.distance_country.iloc[-1][1]} km away.")
+
+    st.line_chart(st.session_state.distance_country, x = 'Country', y = 'Distance')
 
 
-st.write(f"Average amount of guesses/inputs needed: {st.session_state.stats_df['TotalGuesses'].mean()}")
-st.write(f"Average amount of hints needed:{st.session_state.stats_df['Hints'].mean()}")
-
+st.write(f"Average amount of inputs needed: {round(st.session_state.stats_df['TotalInputs'].mean(), 2)}")
+st.write(f"Average amount of hints needed: {round(st.session_state.stats_df['Hints'].mean(), 2)}")
+st.write(f"Average amount of questions needed: {round(st.session_state.stats_df['QuestionAsked'].mean(), 2)}")
+st.write(f"Average amount of country guesses needed: {round(st.session_state.stats_df['CountryGuesses'].mean(), 2)}")
 
 
 # creating line chart (we do not know why it is sorted alphabetically)
-st.line_chart(st.session_state.stats_df, x = 'Country', y = ['TotalGuesses','Hints'])
-
+st.bar_chart(st.session_state.stats_df, x = 'Country', y = ['TotalInputs','Hints','QuestionAsked', 'CountryGuesses'], stack = False)
+st.line_chart(st.session_state.stats_df, x = 'Country', y = ['DistanceTraveled'])
 
 st.write(st.session_state.stats_df)
+
